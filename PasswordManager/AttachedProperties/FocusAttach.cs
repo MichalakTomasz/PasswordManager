baa398f762689de640c042387f085237fc0e4e59
Tomasz Michalak <vlad3dracula@gmail.com>
2020-12-18 13:59:31 +0100

    Added Attached property FocusAttach

---
+++ PasswordManager/AttachedProperties/FocusAttach.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace PasswordManager.AttachedProperties
+{
+    public class FocusAttach
+    {
+
+
+        public static bool GetIsFocused(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(IsFocusedProperty);
+        }
+
+        public static void SetIsFocused(DependencyObject obj, bool value)
+        {
+            obj.SetValue(IsFocusedProperty, value);
+        }
+
+        public static readonly DependencyProperty IsFocusedProperty =
+            DependencyProperty.RegisterAttached("IsFocused", typeof(bool), typeof(FocusAttach), new PropertyMetadata(false, OnIsFocusedChanged));
+
+        private static void OnIsFocusedChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var uiElement = d as UIElement;
+            if (uiElement != null)
+                if ((bool)e.NewValue)  uiElement.Focus();
+        }
+    }
+}
