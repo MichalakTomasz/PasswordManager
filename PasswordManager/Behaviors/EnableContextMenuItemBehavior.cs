6b4b824fba5ff81bc22247f6e341eb07a59508f6
Tomasz Michalak <vlad3dracula@gmail.com>
2020-12-30 14:12:12 +0100

    Added Enable ContextMenuItems behavior

---
+++ PasswordManager/Behaviors/EnableContextMenuItemBehavior.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xaml.Behaviors;
+using PasswordManager.Models;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PasswordManager.Behaviors
+{
+    public class EnableContextMenuItemBehavior : Behavior<MenuItem>
+    {
+
+
+        public PasswordWrapper SelectedItem
+        {
+            get { return (PasswordWrapper)GetValue(SelectedItemProperty); }
+            set { SetValue(SelectedItemProperty, value); }
+        }
+
+        public static readonly DependencyProperty SelectedItemProperty =
+            DependencyProperty.Register("SelectedItem", typeof(PasswordWrapper), 
+                typeof(EnableContextMenuItemBehavior), new PropertyMetadata(null));
+
+        protected override void OnAttached()
+        {
+            var contextMenu = AssociatedObject.Parent as ContextMenu;
+            if (contextMenu != null)
+            contextMenu.Opened += ContextMenuOpened;
+
+        }
+
+        private void ContextMenuOpened(object sender, RoutedEventArgs e)
+        {
+            AssociatedObject.IsEnabled = SelectedItem != null;
+        }
+    }
+}
